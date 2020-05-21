1d15712c51a0094abaac95d7002a8e7ebe88c852
Jacintha Dreischor <jacinthadreischor@hotmail.com>
2020-05-21 12:45:54 +0200

    aanmaken classes voor class diagram

---
+++ SomerenModel/Drankje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Drankje
+    {
+        public String drankNaam { get; set; } // naam van het drankje
+        public double prijs { get; set; } // prijs van het drankje
+        public String alcholisch { get; set; } // is het drankje alcoholisch (ja of nee)
+        public int aantal { get; set; } // voorraad van drankjes
+    }
+}
